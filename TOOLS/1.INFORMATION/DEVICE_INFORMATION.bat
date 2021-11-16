5f7d3af7c7ea4e863640c402bab328594b8fa214
Dragohn <coff33ninja69@gmail.com>
2021-11-16 14:37:54 +0200

    PLAYED WITH HARDWARE INFO

--- TOOLS/1.INFORMATION/DEVICE_INFORMATION.bat
+++ TOOLS/1.INFORMATION/DEVICE_INFORMATION.bat
@@ -1,75 +1,106 @@
+@echo off
+if %os%==Windows_NT goto WINNT
+goto NOCON
 
-::========================================================================================================================================
+:WINNT
+echo .Using a Windows NT based system
+echo ..%computername%
 
-cls
-ECHO.
-ECHO =============================
-ECHO Running Admin shell
-ECHO =============================
+REM set variables
+set system=
+set manufacturer=
+set model=
+set serialnumber=
+set osname=
+set sp=
+setlocal ENABLEDELAYEDEXPANSION
+set "volume=C:"
+set totalMem=
+set availableMem=
+set usedMem=
+set IPv4=
+set Domain=
 
-:init
-setlocal DisableDelayedExpansion
-set "batchPath=%~0"
-for %%k in (%0) do set batchName=%%~nk
-set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
-setlocal EnableDelayedExpansion
+echo Getting data [Computer: %computername%]...
+echo Please Wait....
 
-:checkPrivileges
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+REM Get Computer Name
+FOR /F "tokens=2 delims='='" %%A in ('wmic OS Get csname /value') do SET system=%%A
 
-:getPrivileges
-if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-ECHO.
-ECHO **************************************
-ECHO Invoking UAC for Privilege Escalation
-ECHO **************************************
+REM Get Computer Manufacturer
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Manufacturer /value') do SET manufacturer=%%A
 
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-ECHO Next >> "%vbsGetPrivileges%"
-ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
-exit /B
+REM Get Computer Model
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
 
-:gotPrivileges
-setlocal & pushd .
-cd /d %~dp0
-if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+REM Get Computer Serial Number
+FOR /F "tokens=2 delims='='" %%A in ('wmic Bios Get SerialNumber /value') do SET serialnumber=%%A
+
+REM Get Computer OS
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get Name /value') do SET osname=%%A
+FOR /F "tokens=1 delims='|'" %%A in ("%osname%") do SET osname=%%A
+
+REM Get Computer OS SP
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get ServicePackMajorVersion /value') do SET sp=%%A
+
+REM Get Memory
+FOR /F "tokens=4" %%a in ('systeminfo ^| findstr Physical') do if defined totalMem (set availableMem=%%a) else (set totalMem=%%a)
+set totalMem=%totalMem:,=%
+set availableMem=%availableMem:,=%
+set /a usedMem=totalMem-availableMem
+
+FOR /f "tokens=1*delims=:" %%i IN ('fsutil volume diskfree %volume%') DO (
+    SET "diskfree=!disktotal!"
+    SET "disktotal=!diskavail!"
+    SET "diskavail=%%j"
+)
+FOR /f "tokens=1,2" %%i IN ("%disktotal% %diskavail%") DO SET "disktotal=%%i"& SET "diskavail=%%j"
 
 
-::========================================================================================================================================
+echo done!
 
-:SYS_INFO
-CLS
-Title SYSTEM INFORMATION
-ECHO:
-ECHO    THIS OPTION DETAILS WINDOWS, HARDWARE, AND NETWORKING CONFIGURATION.
-TITLE My System Info
-ECHO Please wait... Checking system information.
-timeout 5 >nul
+echo --------------------------------------------
+echo System Name: %system%
+echo Manufacturer: %manufacturer%
+echo Model: %model%
+echo Serial Number: %serialnumber%
+echo Operating System: %osname%
+echo C:\ Total: %disktotal:~0,-9% GB
+echo C:\ Avail: %diskavail:~0,-9% GB
+echo Total Memory: %totalMem%
+echo Used  Memory: %usedMem%
+echo Computer Processor: %processor_architecture%
+echo Service Pack: %sp%
+echo --------------------------------------------
+
+REM Generate file
+SET file="C:/computername%.txt"
+echo -------------------------------------------- >> %file%
+echo Details For: %system% >> %file%
+echo Manufacturer: %manufacturer% >> %file%
+echo Model: %model% >> %file%
+echo Serial Number: %serialnumber% >> %file%
+echo Operating System: %osname% >> %file%
+echo C:\ Total: %disktotal:~0,-9% GB
+echo C:\ Avail: %diskavail:~0,-9% GB
+echo Total Memory: %totalMem% >> %file%
+echo Used  Memory: %usedMem% >> %file%
+echo Computer Processor: %processor_architecture% >> %file%
+echo Service Pack: %sp% >> %file%
+echo -------------------------------------------- >> %file%
+
+echo %system%, %model%, %serialnumber%, %osname%, >> hardware-audit.csv
+REM systeminfo | findstr /c:"Host Name" /c:"OS Name" /c:"OS Version" /c:"System Manufacturer" /c:"System Model" /c:"System type" /c:"Total Physical Memory" /c:"Domain"
+
+REM request user to push any key to continue
 pause
-:: Section 1: Windows information
-ECHO ==========================
-ECHO WINDOWS INFO
-ECHO ============================
-systeminfo | findstr /c:"OS Name"
-systeminfo | findstr /c:"OS Version"
-systeminfo | findstr /c:"System Type"
-:: Section 2: Hardware information.
-ECHO ============================
-ECHO HARDWARE INFO
-ECHO ============================
-systeminfo | findstr /c:"Total Physical Memory"
-wmic cpu get name
-wmic diskdrive get name,model,size
-wmic path win32_videocontroller get name
-:: Section 3: Networking information.
-ECHO ============================
-ECHO NETWORK INFO
-ECHO ============================
-ipconfig | findstr IPv4
-ipconfig | findstr IPv6
-PAUSE & CLS & GOTO MainMenu
+
+goto END
+
+:NOCON
+echo Error...Invalid Operating System...
+echo Error...No actions were made...
+goto END
+
+:END
+PAUSE
