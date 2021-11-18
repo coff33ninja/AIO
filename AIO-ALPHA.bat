c176adde8cb26797193ff56740756d817fa5d31d
Dragohn <coff33ninja69@gmail.com>
2021-11-18 13:43:15 +0200

    TINY MISTAKE

--- AIO-ALPHA.bat
+++ AIO-ALPHA.bat
@@ -633,7 +633,7 @@ color 0f
 mode con cols=98 lines=60
 ECHO THE FILE HERE WILL BE CHANGED INTO MULTIPLE PACKS AND TRIGGERS STAY TUNED
 powershell.exe Invoke-WebRequest "https://raw.githubusercontent.com/coff33ninja/AIO/main/TOOLS/2.COMPUTER_CONFIGURATION/MAS.cmd" -O "c:\aio\MAS.cmd"
-start wait c:\aio\MAS.cmd
+start /wait c:\aio\MAS.cmd
 PAUSE 
 
 ::========================================================================================================================================
