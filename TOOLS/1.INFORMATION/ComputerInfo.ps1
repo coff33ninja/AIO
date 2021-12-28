9bcc4f7a17075710d94b0b81b964a4ed59bac996
Dragohn <51002754+coff33ninja@users.noreply.github.com>
2021-12-28 20:55:27 +0200

    test 3

--- TOOLS/1.INFORMATION/ComputerInfo.ps1
+++ TOOLS/1.INFORMATION/ComputerInfo.ps1
@@ -172,7 +172,7 @@ function CustomInputBox([string] $title, [string] $message, [string] $defaultTex
 
 
 $userInput = CustomInputBox "User Name" "Please enter your name." ""
-if ( $userInput -ne $null ) {
+if ($null -ne $userInput) {
     $Report | Out-File .\[$userInput]-Basic-Computer-Information-Report.html
 }
 else {
