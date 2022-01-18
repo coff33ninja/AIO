0d032fcc88f9f639e05a60dc6b14550854fd6ffd
Dragohn <51002754+coff33ninja@users.noreply.github.com>
2022-01-18 07:55:25 +0200

    changed header to show what package version this is

--- AIO.cmd
+++ AIO.cmd
@@ -58,7 +58,7 @@ echo:
 echo                       Press the corresponding number to go to desired section:
 echo:
 echo                  ^|===============================================================^|
-echo                  ^|                       AIO v1.6 MAIN MENU                      ^| 
+echo                  ^|               AIO v1.6 aria2 version MAIN MENU                ^| 
 echo                  ^|      ___________________________________________________      ^|
 echo                  ^|                                                               ^|
 echo                  ^|      [1] INFORMATION                                          ^|
