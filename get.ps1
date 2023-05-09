7ce329b52a9468040b995ae5e4f758db7644b0e9
Dragohn <51002754+coff33ninja@users.noreply.github.com>
2023-05-09 08:38:09 +0200

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -2,12 +2,12 @@ $ErrorActionPreference = 'Stop'
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://github.com/coff33ninja/AIO/blob/testing-irm-new-layout/AIO_ARIA_VERSION.cmd'
-$DownloadURL2 = 'https://raw.githubusercontent.com/coff33ninja/AIO/testing-irm-new-layout/AIO_ARIA_VERSION.cmd'
+$DownloadURL = 'https://github.com/coff33ninja/AIO/blob/testing-irm-new-layout/AIO.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/coff33ninja/AIO/testing-irm-new-layout/AIO.cmd'
 
 $rand = Get-Random -Maximum 1000
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\AIO_ARIA_VERSION_$rand.cmd" } else { "$env:TEMP\AIO_ARIA_VERSION_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\AIO_$rand.cmd" } else { "$env:TEMP\AIO_$rand.cmd" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
@@ -23,5 +23,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\AIO_ARIA_VERSION*.cmd", "$env:SystemRoot\Temp\AIO_ARIA_VERSION*.cmd")
+$FilePaths = @("$env:TEMP\AIO*.cmd", "$env:SystemRoot\Temp\AIO*.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
